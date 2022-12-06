973f8306b148169f752d4e911971e6355a986768
vuxuanthanh1203 <vuxuanthanh1203@gmail.com>
2022-12-06 15:32:13 +0700

    update graphql code convention

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,41 +1,44 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type User {
-        id: ID
-        name: String
-        email: String
+# ROOT TYPE
+    type Query {
+        categories: [Category]!
+        category (id: ID!): Category!
+
+        products: [Product]!
+        product (id: ID!): Product!
+
+        users: [User]!
+        user (id: ID!): User!
+        
+    }
+
+    type Mutation {
+        createCategory(name: String!): Category!
+        deleteCategory(id:ID!): Boolean!
+        updateCategory(id:ID!,name:String): Boolean!
+
+        createProduct(name: String!, des: String!, categoryId: ID!): Product!
     }
 
     type Category {
-        id: ID
-        name: String
+        id: ID!
+        name: String!
         products: [Product]
     }
 
     type Product {
-        id: ID
-        name: String
-        des: String
-        category: Category
-    }
- 
-    # ROOT TYPE
-    type Query {
-        products: [Product]
-        product (id: ID!): Product
-        users: [User]
-        user (id: ID!): User
-        categories: [Category]
-        category (id: ID!): Category
+        id: ID!
+        name: String!
+        des: String!
+        category: Category!
     }
 
-    type Mutation {
-        createCategory(name: String!): Category
-        deleteCategory(id:ID!): Boolean,
-        updateCategory(id:ID!,name:String): Boolean,
-        createProduct(name: String!, des: String!, categoryId: ID!): Product
+    type User {
+        id: ID!
+        name: String!
+        email: String!
     }
-    
 `
 module.exports = typeDefs;
